6f56dc2a96444a657d816273b9f65570aeeadceb
Callcium13 <callumjohncoffey@gmail.com>
2020-09-21 20:12:29 +0000

    Added nth root option, rounded answer to significant places

--- main.py
+++ main.py
@@ -4,10 +4,18 @@ while num < 0:
     #ask the number to get the root of
     num = (int(input("Number?: ")))
 
-res = -1
-while res <= 0:
-    #ask the resoloution of the approximation
-    res = (float(input("Resoloution? (eg. 1, 0.1, 0.5): ")))
+res_p = -1
+while res_p <= 1:
+    #ask the how many decimal places accuracy
+    res_p = (int(input("Decimal Places? (eg. 1, 2, 5): ")))
+
+#convert decimal places to step size
+res = 1 / (10**res_p)
+
+nth = -1
+while nth < 0:
+    #ask the number to get the root of
+    nth = (int(input("nth root n=?: ")))
 
 #calculate square root X
 i = 0
@@ -18,12 +26,13 @@ while (x <= num):
     for j in range(2):
         x = x * i
 
+#Find closest fit to terminal count
 if ((i + 1)**2 == num):
     square_X = i
 elif ((i + 1)**2 - num) < (num - (i + 1)**2):
-    square_X = i - res
-elif ((i + 1)**2 - num) > (num - (i + 1)**2):
     square_X = i + res
+elif ((i + 1)**2 - num) > (num - (i + 1)**2):
+    square_X = i - res
 else:
     square_X = 'ERROR'
 
@@ -38,12 +47,36 @@ while (x <= num):
 
 if ((i + 1)**3 == num):
     cube_X = i
-elif ((i + 1)**3 - num) < (num - (i + 1)**3):
+elif ((i + 1)**3 - num) <= (num - (i + 1)**3):
     cube_X = i + res
 elif ((i + 1)**3 - num) > (num - (i + 1)**3):
     cube_X = i - res
 else:
     cube_X = 'ERROR'
 
+#calculate nth root
+i = 0
+x = 0
+while (x <= num):
+    i = i + res
+    x = 1
+    for j in range(nth):
+        x = x * i
+
+if ((i + 1)**nth == num):
+    nth_X = i
+elif ((i + 1)**nth - num) <= (num - (i + 1)**nth):
+    nth_X = i + res
+elif ((i + 1)**nth - num) > (num - (i + 1)**nth):
+    nth_X = i - res
+else:
+    nth_X = 'ERROR'
+
+#round off to place count
+square_X = round(square_X, res_p)
+cube_X = round(cube_X, res_p)
+nth_X = round(nth_X, res_p)
+
 print('Square root approximation=', square_X)
 print('Cube root approximation=', cube_X)
+print('nth root approximation=', nth_X)
